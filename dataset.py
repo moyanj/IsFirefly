9ea204d875276939d6326b569a658c0b83f2aa7f
moyan <1561515308@qq.com>
2025-01-25 01:57:24 +0800

    perf(eval): 优化模型预测流程并提升用户体验
    - 添加 predict_transform 用于预测时的图像预处理
    - 更新模型路径并优化模型加载过程
    - 简化预测结果输出，提升可读性
    - 添加中文化页面标题和描述，增强用户体验
    - 新增检测、停止和清除按钮，方便用户操作

--- dataset.py
+++ dataset.py
@@ -10,4 +10,12 @@ transform = transforms.Compose(
     ]
 )
 
+predict_transform = transforms.Compose(
+    [
+        transforms.Resize((512, 512)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
+
 dataset = ImageFolder("preprocessed", transform=transform)
--- eval.py
+++ eval.py
@@ -1,22 +1,24 @@
-from dataset import transform, dataset
+from dataset import predict_transform, dataset
 from model import Model
 from PIL import Image
 import torch
 import gradio as gr
 
 # 加载模型
-model_path = "./model/res152/Model_0.1077(1027图).pth"
+model_path = "./model/res152/Model_87.18_1248.pth"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = Model(2)
 model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
 model.to(device)
 model.eval()
+print("Model loaded successfully.")
+id_to_class = {0: "是", 1: "不是"}
 
 
 # 定义预测函数
 def predict(img):
     try:
-        img = transform(img)  # 应用预处理
+        img = predict_transform(img)  # 应用预处理
         img = img.unsqueeze(0).to(device)  # 添加批量维度并移动到设备
         with torch.no_grad():
             output = model(img)
@@ -26,12 +28,10 @@ def predict(img):
             confidence = probabilities[0].tolist()
 
             # 格式化置信度为字符串
-            confidence_str = ", ".join(
-                [f"Class {i}: {p:.2f}" for i, p in enumerate(confidence)]
-            )
+            confidence_str = f"{confidence[predicted_class]*100:.4f}%"
 
             # 解析结果
-            result = [predicted_class, confidence_str]
+            result = [id_to_class[int(predicted_class)], confidence_str]
             return result
     except Exception as e:
         print(f"Error during prediction: {e}")
@@ -43,11 +43,14 @@ iface = gr.Interface(
     fn=predict,  # 预测函数
     inputs=gr.Image(type="pil"),  # 输入为 PIL 图像
     outputs=[
-        gr.Text(label="Predicted Class"),  # 显示预测类别
-        gr.Text(label="Confidence"),  # 显示置信度
+        gr.Text(label="预测类别"),  # 显示预测类别
+        gr.Text(label="置信度"),  # 显示置信度
     ],
-    title="Image Classifier",  # 页面标题
-    description="Upload an image to classify it using the trained model.",  # 页面描述
+    title="是萤宝吗？",  # 页面标题
+    description="上传一张图片，看看是不是萤宝",  # 页面描述
+    submit_btn="检测",  # 提交按钮文本
+    stop_btn="停止",
+    clear_btn="清除",
 )
 
 # 启动 Gradio 应用
